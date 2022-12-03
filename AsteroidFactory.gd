0c6ba1a76c72e6bce02f43e98d64fe55282e2855
Tristan <thrawst88@gmail.com>
2022-12-03 13:50:00 -0500

    more arguments to spawn asteroid

--- AsteroidFactory.gd
+++ AsteroidFactory.gd
@@ -5,8 +5,9 @@ export(PackedScene) var asteroid_scene = preload("res://Asteroid.tscn")
 const speed_bounds = 10
 var rng = RandomNumberGenerator.new()
 
-func spawn(spawn_global_position, velocity = null):
+func spawn(spawn_global_position, velocity = null, rot_velocity = null):
 	var instance = asteroid_scene.instance()
+	# set velocity vector
 	if velocity == null:
 		instance.start_speed = Vector2(
 			rng.randf_range(-speed_bounds, speed_bounds),
@@ -14,7 +15,12 @@ func spawn(spawn_global_position, velocity = null):
 		)
 	else:
 		instance.start_speed = velocity
-	instance.start_rot = rng.randf_range(-2, 2)
+	# set rotation speed
+	if rot_velocity == null:
+		instance.start_rot = rng.randf_range(-2, 2)
+	else:
+		instance.start_rot = rot_velocity
+		
 	instance.start_pos = spawn_global_position
 	instance.global_position = spawn_global_position
 	instance.transform.origin = spawn_global_position
